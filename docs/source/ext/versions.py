fdcdea8bd1360fc956d0c5732f8f974d83955da3
Jaime Melis <jmelis@opennebula.org>
2014-10-24 13:40:51 +0200

    enable downloads variable in views

--- docs/source/ext/versions.py
+++ docs/source/ext/versions.py
@@ -1,12 +1,14 @@
 def setup(app):
     app.add_config_value('versions', False, 'html')
+    app.add_config_value('downloads', False, 'html')
 
     app.connect('html-page-context', create_versions)
 
 def create_versions(app, docname, templatename, ctx, doctree):
-    ctx['versions'] = app.builder.env.config['versions']
-    ctx['display_github'] = True
+    ctx['versions']  = app.builder.env.config['versions']
+    ctx['downloads'] = app.builder.env.config['downloads']
 
+    ctx['display_github'] = True
     ctx['github_user']    = 'OpenNebula'
     ctx['github_repo']    = 'vOneCloud'
     ctx['github_version'] = 'master/'
