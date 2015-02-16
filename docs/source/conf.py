085e482b0288923ca6c91a88cf1f0d891cfd7e5a
Jaime Melis <jmelis@opennebula.org>
2015-02-16 14:26:06 +0100

    copyright 2015

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'vOneCloud'
-copyright = '2014, OpenNebula Systems'
+copyright = '2015, OpenNebula Systems'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
